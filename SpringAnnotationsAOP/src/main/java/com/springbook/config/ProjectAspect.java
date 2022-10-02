5a072cadbf68fb6d281bb982ad4e45eef7b26481
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-10-02 23:29:01 +0530

    Adding the aspect implementations to the project

--- SpringAnnotationsAOP/src/main/java/com/springbook/config/ProjectAspect.java
+++ SpringAnnotationsAOP/src/main/java/com/springbook/config/ProjectAspect.java
@@ -1,9 +1,22 @@
 package com.springbook.config;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
 
 @Component
 @Aspect
 public class ProjectAspect {
+
+    private final Logger logger = Logger.getLogger(ProjectAspect.class.getName());
+
+    @Around("@annotation(ToLog)")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object returnValue = joinPoint.proceed();
+        logger.info("Watching from inside the method xD!");
+        return returnValue;
+    }
 }
