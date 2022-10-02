d88c7c827409f01e58220784cc76894d2c5fea7e
prateek-parashar <36658243+prateek-parashar@users.noreply.github.com>
2022-10-02 23:23:19 +0530

    Add the custom annotatin and add log statements to the service methods

---
+++ SpringAnnotationsAOP/src/main/java/com/springbook/config/ProjectAspect.java
@@ -0,0 +1,9 @@
+package com.springbook.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ProjectAspect {
+}
--- SpringAnnotationsAOP/src/main/java/com/springbook/service/CommentService.java
+++ SpringAnnotationsAOP/src/main/java/com/springbook/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.springbook.service;
 
+import com.springbook.config.ToLog;
 import com.springbook.model.Comment;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +10,17 @@ import java.util.logging.Logger;
 public class CommentService {
 
     Logger logger = Logger.getLogger(CommentService.class.getName());
-    public void publishComment(Comment comment) {
 
+    public void publishComment(Comment comment) {
+        logger.info("Publishing the comment by author " + comment.getAuthor());
     }
 
     public void editComment(Comment comment) {
-
+        logger.info("Editing the comment by author " + comment.getAuthor());
     }
 
+   @ToLog
     public void deleteComment(Comment comment) {
-
+        logger.info("Deleting the comment by author " + comment.getAuthor());
     }
 }
